0ce67007fa3662e8dca7a5075438add0c33afd11
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-02-09 15:04:50 -0800

    add starting bboard methods

--- w4d2/chess/board.rb
+++ w4d2/chess/board.rb
@@ -1,3 +1,21 @@
 class Board
+  def initialize
+    @rows = Array.new(8) { Array.new(8) }
+  end
+
+  def [](pos)
+    x, y = pos
+    @rows[x][y]
+  end
+
+  def []=(pos, val)
+    x, y = pos
+    @rows[x][y] = val
+  end
+
+  def move_piece(start_pos, end_pos)
+    # replace start_pos with null piece
+    @rows[end_pos] = @rows[start_pos]
+  end
 
 end
