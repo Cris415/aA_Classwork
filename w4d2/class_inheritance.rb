7df8e971ec890a8946c2ac199b545d2e35c3d6ca
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-02-09 14:11:13 -0800

    add employee and manager classes

--- w4d2/class_inheritance.rb
+++ w4d2/class_inheritance.rb
@@ -1,1 +1,31 @@
-puts "hello"
+class Employee
+  attr_reader :name, :title, :salary, :boss
+  def initialize(name, title, salary, boss)
+    @name = name
+    @title = title
+    @salary = salary
+    @boss = boss
+  end
+
+  def bonus(multiplier)
+    salary * multiplier
+  end
+
+
+
+end
+
+class Manager < Employee
+  def initialize(name, title, salary, boss)
+    super
+    @employees = []
+  end
+
+  def bonus(multiplier)
+    salary_sum = @employees.inject do |sum, employee| 
+      sum + employee.salary
+    end
+    salary_sum * multiplier
+  end
+
+end
