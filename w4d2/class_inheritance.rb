98ebf9d0a842624b6033c7321c104f7ea86f5367
Taylor Musolf <tcmusolf@gmail.com>
2021-02-09 14:41:31 -0800

    Add bonus structure

--- w4d2/class_inheritance.rb
+++ w4d2/class_inheritance.rb
@@ -16,16 +16,30 @@ class Employee
 end
 
 class Manager < Employee
+  attr_reader :employees
   def initialize(name, title, salary, boss)
     super
     @employees = []
   end
 
   def bonus(multiplier)
-    salary_sum = @employees.inject do |sum, employee| 
-      sum + employee.salary
-    end
-    salary_sum * multiplier
+    sum = 0
+    @employees.each {|employee| sum += employee.salary}
+    sum * multiplier
   end
 
+  def add_employee(employee)
+    @employees << employee
+  end
 end
+
+m = Manager.new("Bob", "VP", 100, nil)
+e1 = Employee.new("Taylor", "Junior VP", 50, "Bob")
+e2 = Employee.new("Cristian", "Junior", 75, m)
+m.add_employee(e1)
+m.add_employee(e2)
+m.employees.each{|el| puts el.salary }
+e1.salary
+p e1.bonus(0.25)
+p e2.bonus(0.50)
+p m.bonus(0.50)
