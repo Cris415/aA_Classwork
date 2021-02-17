c16ed4e39b3d0e340053acbacd642a00dd6424b9
Jason Li <li.jasontse@gmail.com>
2021-02-17 15:41:05 -0800

    create questions.db and database class

--- W5D3/aa_questions/import_db.sql
+++ W5D3/aa_questions/import_db.sql
@@ -23,7 +23,7 @@ CREATE TABLE questions (
 DROP TABLE IF EXISTS questions_follows;
 
 CREATE TABLE questions_follows (
-  -- id INTEGER PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   users_id INTEGER NOT NULL,
   questions_id INTEGER NOT NULL,
 
@@ -37,27 +37,62 @@ CREATE TABLE questions_follows (
           -- 4             13
           -- 4             18
 
+DROP TABLE IF EXISTS replies;
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    questions_id INTEGER NOT NULL,
+    reply_key INTEGER,
+    users_reply_id INTEGER NOT NULL,
+    body TEXT NOt NULL, 
+    top_level INTEGER NOT NULL, 
+
+
+    FOREIGN KEY (questions_id) REFERENCES questions(id)
+    FOREIGN KEY (reply_key) REFERENCES replies(id)
+    FOREIGN KEY (users_reply_id) REFERENCES users(id)
+
+);
+
+DROP TABLE IF EXISTS question_likes;
+
+CREATE TABLE question_likes (
+    questions_id INTEGER NOT NULL,
+    users_id INTEGER NOT NULL,
+
+    FOREIGN KEY (questions_id) REFERENCES questions(id)
+    FOREIGN KEY (users_id) REFERENCES users(id)
+);
+
 
 INSERT INTO
   users (fname, lname)
 VALUES
   ('Joe', 'Biden'),
-  ('Melissa', 'Carter');
+  ('Melissa', 'Carter'),
+  ('Barack', 'Obama');
 
 INSERT INTO
   questions (title, body, users_id)
 VALUES
   ('How to change variable', 'how to do it in ruby', (SELECT id FROM users WHERE fname = 'Joe' AND lname = 'Biden' )),
   ('URGENT', 'NEED HELP!', (SELECT id FROM users WHERE fname = 'Melissa' AND lname = 'Carter' )),
-  ('TESTING', 'STILL NEED HELP', (SELECT id FROM users WHERE fname = 'Melissa' AND lname = 'Carter' ));
+  ('TESTING', 'STILL NEED HELP', (SELECT id FROM users WHERE fname = 'Melissa' AND lname = 'Carter' )),
+  ('NEW QUESTION', 'AMERICA', (SELECT id FROM users WHERE fname = 'Barack' AND lname = 'Obama' ));
 
 INSERT INTO
   questions_follows (users_id, questions_id )
 VALUES
-  ((SELECT id FROM users WHERE fname = 'Melissa' AND lname = 'Carter' ),
-  (SELECT id FROM questions WHERE users_id = (SELECT id FROM users WHERE fname = 'Melissa' AND lname = 'Carter'))
-  ),
+  (2, 2),
+--   (SELECT id FROM questions WHERE users_id = (SELECT id FROM users WHERE fname = 'Melissa' AND lname = 'Carter')),
   ((SELECT id FROM users WHERE fname = 'Joe' AND lname = 'Biden' ),
   (SELECT id FROM questions WHERE users_id = (SELECT id FROM users WHERE fname = 'Joe' AND lname = 'Biden'))
   );
+
+INSERT INTO
+    replies (questions_id, reply_key, users_reply_id, body, top_level)
+VALUES
+    (1, NULL, 2, "REPLIES", 1),
+    (1, 1, 3, "RESPONSE", 2);
+
   
