cb3fe2a53a36013daddb9cedf1b73c593d5198ca
Nightwatch <rdmc0072@yahoo.com>
2021-03-08 10:09:31 -0800

    completed controllers

--- w8d1/reddit_clone/app/controllers/sessions_controller.rb
+++ w8d1/reddit_clone/app/controllers/sessions_controller.rb
@@ -1,2 +1,26 @@
 class SessionsController < ApplicationController
+
+    before_action(:require_signed_in, only:[:destroy])
+    
+    def create
+        @user = User.find_by_credentials(params[:user][:username],params[:user][:password])
+
+        if @user
+            sign_in!(@user)
+            redirect_to(user_url(@user))
+        else
+            flash[:errors] = ["Invalid Username or Password"]
+            redirect_to(new_session_url)
+        end
+    end
+
+    def new
+        @user User.new
+        render(:new)
+    end
+
+    def destroy
+        sign_out!
+        redirect_to(:new_session_url)
+    end
 end
--- w8d1/reddit_clone/app/controllers/users_controller.rb
+++ w8d1/reddit_clone/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action(:require_signed_in, only:[:show,:destroy])
+
   def new
     @user = User.new
 
@@ -19,5 +21,32 @@ class UsersController < ApplicationController
     render :show
   end
 
+  def index
+    @users = User.all
+    render(:index)
+  end
+
+  def destroy
+    user = User.find_by(id: params[:id])
+
+    user.destroy
+
+    redirect_to(users_url)
+  end
+
+  def edit
+     @user = User.new
+     render(:edit)
+  end
+
+  def update
+     @user = User.find_by(id: params[:id])
+
+     if @user.update(params.require(:user).permit(:username, :password))
+      redirect_to(user_url(@user))
+     else
+      redirect_to(edit_user_url(@user))
+     end
+  end
 
 end
