e6038d43552bb96a131b898bbbf2b36548dc7686
Cristian Reynoso <cristianreynoso91@gmail.com>
2021-03-09 15:25:56 -0800

    complete phase 1

--- w8d2/rails_lite/bin/p01_basic_server.rb
+++ w8d2/rails_lite/bin/p01_basic_server.rb
@@ -1,1 +1,18 @@
 require 'rack'
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  if req.path == "/hello/word/cool"
+    res.write("hello world cool")
+  else
+    res.write("path does not match secret path #{req.path}")
+  end
+  res.finish
+end
+
+Rack::Server.start(
+  app:app,
+  Port: 3000
+)
